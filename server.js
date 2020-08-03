c29ff1bc40064dc232f96e6edc49b5b021854171
chrisbefin <christian@jba4.com>
2020-08-03 12:10:27 -0600

    attempted solution

--- server.js
+++ server.js
@@ -15,19 +15,35 @@
 // //listen for requests
 // app.listen(process.env.PORT || 8080);
 
-//Install express server
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+//
+// const app = express();
+//
+// // Serve only the static files from the dist directory
+// app.use(express.static(__dirname + '/dist/<name-of-app>'));
+//
+// app.get('/*', function(req,res) {
+//
+// res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+// });
+//
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
+
+
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/oobat'));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/oobat' }
+  );
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
