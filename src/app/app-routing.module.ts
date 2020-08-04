7b8da8cfb26a4380877fc3907ce8f425c93904de
chrisbefin <christian@jba4.com>
2020-08-04 12:45:38 -0600

    basic navigation added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HighScoresComponent } from './high-scores/high-scores.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'high-scores', component: HighScoresComponent },
+  { path: 'main-menu', component: MainMenuComponent},
+  { path: '', redirectTo: '/main-menu', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighScoresComponent } from './high-scores/high-scores.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighScoresComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
