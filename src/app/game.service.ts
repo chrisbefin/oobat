6b4472d488b3fc957640256bb3e485ee9328d5b2
chrisbefin <christian@jba4.com>
2020-08-06 17:22:24 -0600

    test

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -15,6 +15,7 @@ export class GameService {
     this.socket.emit("getCard");
     this.socket.on("card", (key,hint1,hint2,hint3,hint4,hint5) => {
       console.log("card received");
+      console.log(key, hint1, hint2, hint3, hint4, hint5);
       return [key,hint1,hint2,hint3,hint4,hint5];
     });
   }
