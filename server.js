714ffa5447968eb4b12cdf39052489e6b48f5c2a
chrisbefin <christian@jba4.com>
2020-08-07 14:27:42 -0600

    add quotes

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ io.on("connection", socket => {
 function getHighScores (gamemode) {
   client.connect();
 
-  client.query(`SELECT name, score FROM scores WHERE gamemode = ${gamemode} ORDER BY score DESC LIMIT(3)`, (err, res) => {
+  client.query(`SELECT name, score FROM scores WHERE gamemode = '${gamemode}' ORDER BY score DESC LIMIT(3)`, (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
       console.log(JSON.stringify(row));
